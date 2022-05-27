1943e0bab0b5be067f9f644086fa4555bd7f3c83
ERI-CHOU <dsayzhanglin@gmail.com>
2022-05-27 11:52:11 +0900

    3.12 Command-line database bug fixed

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if(process.argv.length < 3){
+if (process.argv.length < 3) {
     console.log('Please provide the password as an argument: node mongo.js <password>')
     process.exit(1)
 }
@@ -18,26 +18,26 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-if(process.argv.length === 3){
+if (process.argv.length === 3) {
     Person.find({}).then(result => {
         result.forEach(person => {
             console.log(person)
             mongoose.connection.close()
         })
     })
-    
-}else if(process.argv.length === 5){
+
+} else if (process.argv.length === 5) {
     const person = new Person({
         name: process.argv[3],
-        number: "13-837525795"
+        number: process.argv[4]
     })
-    
+
     person.save().then(result => {
         console.log('person saved!')
         mongoose.connection.close()
     })
 
-}else{
+} else {
     mongoose.connection.close()
     console.log('Wrong number of arguments')
 }
