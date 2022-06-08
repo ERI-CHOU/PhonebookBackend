27026d5630784723396b5abd75bbb6eb9514fff7
ERI-CHOU <dsayzhanglin@gmail.com>
2022-06-09 08:56:24 +0900

    fullbuild

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
-const { response } = require('express')
 
 const app = express()
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
-morgan.token('body', (request, response) => {
+morgan.token('body', request => {
   return JSON.stringify(request.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-const generateId = () => Math.floor(Math.random() * 100000)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!<h1>')
@@ -50,7 +47,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -71,22 +68,22 @@ app.post('/api/persons', (request, response, next) => {
   }
 
   const person = new Person({
-    "name": body.name,
-    "number": body.number,
+    'name': body.name,
+    'number': body.number,
   })
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
-    "name": body.name,
-    "number": body.number,
+    'name': body.name,
+    'number': body.number,
   }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
@@ -107,8 +104,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError'){
-    return response.status(400).json({error: error.message})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -5,39 +5,39 @@ const url = process.env.MONGOOB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('conneted to MoogoBD')
-    })
-    .catch(error => {
-        console.log('error connecting to MongDB', error.message)
-    })
+  .then(() => {
+    console.log('conneted to MoogoBD')
+  })
+  .catch(error => {
+    console.log('error connecting to MongDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    maxlength: 31,
+    validate: {
+      validator: function (v) {
+        return /^[0-9]{2,3}-[0-9]{4,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid number`
     },
-    number: {
-        type: String,
-        minlength: 8,
-        maxlength: 31,
-        validate: {
-            validator: function(v){
-                return /^[0-9]{2,3}-[0-9]{4,}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid number`
-        },
-        required: [true, 'number required']
-    }
+    required: [true, 'number required']
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
